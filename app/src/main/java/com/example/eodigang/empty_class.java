ef3cb81375ed16b7f8db66718df4cb8e3e5b0a60
janghwal <jhp337037@gmail.com>
2023-05-20 15:10:31 +0900

    eodigang init

---
+++ app/src/main/java/com/example/eodigang/empty_class.java
@@ -0,0 +1,14 @@
+package com.example.eodigang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class empty_class extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty_class);
+    }
+}
